a42a03b5d163cbb7aee7811bcd850ee4976c1a65
Abby Nyakara <abbynyakara@gmail.com>
2022-12-05 15:28:45 +0300

    correct linters

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import Greeting from "./components/greeting";
-import store from './redux/configureStore';
-
 import {
   BrowserRouter as Router,
   Route,
-  Routes
+  Routes,
 } from "react-router-dom";
+import Greeting from './components/greeting';
+import store from './redux/configureStore';
 
 function App() {
   return (
     <Provider store={store}>
       <Router>
         <Routes>
-          <Route exact path="/" element={<Greeting />}/>
+          <Route exact path="/" element={<Greeting />} />
         </Routes>
       </Router>
     </Provider>
